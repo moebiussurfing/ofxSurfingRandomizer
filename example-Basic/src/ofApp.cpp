5fb8a3725d34838264ffc4351a10ca57e63c926b
moebius surfing <moebiussurfing@gmail.com>
2021-05-22 22:08:05 -0300

    +

--- example-Basic/src/ofApp.cpp
+++ example-Basic/src/ofApp.cpp
@@ -5,32 +5,58 @@ void ofApp::setup()
 {
 	ofSetLogLevel(OF_LOG_VERBOSE);
 
-	//required call
-	ImGuiConfigFlags flags;
-	flags = ImGuiConfigFlags_DockingEnable;
-	bool bMouse = false;//false to auto show on window workflow
+	setup_ImGui();
+
+	// parameters to randomize
+	params.setName("paramsGroup");// main container
+	params2.setName("paramsGroup2");// nested
+	params3.setName("paramsGroup3");// nested
+	params.add(lineWidth.set("lineWidth", 0.5, 0, 1));
+	params.add(separation.set("separation", 50, 1, 100));
+	params.add(speed.set("speed", 0.5, 0, 1));
+	params.add(shapeType.set("shapeType", 0, -50, 50));
+	params.add(size.set("size", 100, 0, 100));
+	params.add(amount.set("amount", 10, 0, 25));
+	params2.add(shapeType2.set("shapeType2", 0, -50, 50));
+	params2.add(size2.set("size2", 100, 0, 100));
+	params2.add(amount2.set("amount2", 10, 0, 25));
+	params3.add(lineWidth3.set("lineWidth3", 0.5, 0, 1));
+	params3.add(separation3.set("separation3", 50, 1, 100));
+	params3.add(speed3.set("speed3", 0.5, 0, 1));
+	params2.add(params3);
+	params.add(params2);
+
+	data.setup(params);
+}
+//--------------------------------------------------------------
+void ofApp::setup_ImGui()
+{
+	ImGuiConfigFlags flags = ImGuiConfigFlags_DockingEnable;
 	bool bAutoDraw = true;
 	bool bRestore = true;
+	bool bMouse = false;
 	gui.setup(nullptr, bAutoDraw, flags, bRestore, bMouse);
 
-	params.setName("sceneParamsGroup");// main group 
-	paramsNested1.setName("styleSubGroup");// another nested group
-	paramsNested2.setName("itemSubGroup");// another nested group
+	auto &io = ImGui::GetIO();
+	auto normalCharRanges = io.Fonts->GetGlyphRangesDefault();
 
-	paramsNested1.add(fill.set("fill", true));
-	paramsNested1.add(color.set("color", ofColor(0, 255), ofColor(0, 0), ofColor(255, 255)));
-	paramsNested1.add(lineWidth.set("lineWidth", 1, 0.1, 10));
+	//-
 
-	paramsNested2.add(shapeType.set("shapeType", 1, 1, 2));
-	paramsNested2.add(size.set("size", 100, 5, 200));
-	paramsNested2.add(paramsNested1);
+	// font
+	std::string fontName;
+	float fontSizeParam;
+	fontName = "telegrama_render.otf"; //  WARNING: will crash if font not present!
+	fontSizeParam = 11;
 
-	params.add(amount.set("amount", 10, 1, 24));
-	params.add(separation.set("separation", 10, 1, 100));
-	params.add(paramsNested2);
+	//-
 
+	std::string _path = "assets/fonts/"; // assets folder
+	customFont = gui.addFont(_path + fontName, fontSizeParam, nullptr, normalCharRanges);
+	io.FontDefault = customFont;
 
-	randomizerGroup.setup(params);
+	// theme
+	ofxSurfingHelpers::ImGui_ThemeMoebiusSurfing();
+	//ofxSurfingHelpers::ImGui_ThemeModernDark();
 }
 
 //--------------------------------------------------------------
@@ -39,36 +65,63 @@ void ofApp::update() {
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-	auto mainSettings = ofxImGui::Settings();
 
 	gui.begin();
 	{
-		ImGuiCond flagsw;
-		flagsw = ImGuiCond_Once;
+		bLockMouseByImGui = false;
 
+		//panels sizes
+		float xx = 10;
+		float yy = 10;
 		float ww = PANEL_WIDGETS_WIDTH;
-		float hh = 600;
-		float pad = 10;
+		float hh = PANEL_WIDGETS_HEIGHT;
 
-		ImGui::SetNextWindowPos(ImVec2(10, 10), flagsw);
-		ImGui::SetNextWindowSize(ImVec2(ww, hh), flagsw);
-		if (ofxImGui::BeginWindow("RANDOMIZER", mainSettings, flagsw))
+		//widgets sizes
+		float _spcx;
+		float _spcy;
+		float _w100;
+		float _h100;
+		float _w99;
+		float _w50;
+		float _w33;
+		float _w25;
+		float _h;
+
+		static bool auto_resize = true;
+		ImGuiWindowFlags flagsw = auto_resize ? ImGuiWindowFlags_AlwaysAutoResize : ImGuiWindowFlags_None;
+
+		ImGui::PushFont(customFont);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2(ww, hh));
 		{
-			randomizerGroup.drawWidgets();
-		}
-		ofxImGui::EndWindow(mainSettings);
+			ImGui::SetNextWindowPos(ImVec2(10, 10), flagsw);
+			ImGui::SetNextWindowSize(ImVec2(ww, hh), flagsw);
+			if (ofxImGui::BeginWindow("RANDOMIZER", mainSettings, flagsw))
+			{
+				ofxSurfingHelpers::refreshImGui_WidgetsSizes(_spcx, _spcy, _w100, _h100, _w99, _w50, _w33, _w25, _h);
+				data.drawWidgets();
+			}
+			ofxImGui::EndWindow(mainSettings);
 
+			//--
 
-		ImGui::SetNextWindowPos(ImVec2(300, 10), flagsw);
-		ImGui::SetNextWindowSize(ImVec2(ww, hh), flagsw);
-		if (ofxImGui::BeginWindow("PARAMETERS", mainSettings, flagsw))
-		{
-			ofxImGui::AddGroup(params, mainSettings);
+			ImGui::SetNextWindowPos(ImVec2(300, 10), flagsw);
+			ImGui::SetNextWindowSize(ImVec2(ww, hh), flagsw);
+			if (ofxImGui::BeginWindow("PARAMETERS", mainSettings, flagsw))
+			{
+				ofxSurfingHelpers::refreshImGui_WidgetsSizes(_spcx, _spcy, _w100, _h100, _w99, _w50, _w33, _w25, _h);
+				ofxImGui::AddGroup(params, mainSettings);
+			}
+			ofxImGui::EndWindow(mainSettings);
 		}
-		ofxImGui::EndWindow(mainSettings);
+		ImGui::PopStyleVar();
+		ImGui::PopFont();
 	}
-	gui.end();
+	//mouse lockers
+	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow);
+	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_RootAndChildWindows);
+	bLockMouseByImGui = bLockMouseByImGui | ImGui::IsWindowHovered(ImGuiHoveredFlags_AllowWhenBlockedByActiveItem);
 
+	gui.end();
 }
 
 
--- example-Basic/src/ofApp.h
+++ example-Basic/src/ofApp.h
@@ -9,22 +9,33 @@ public:
     void setup();
     void update();
     void draw();
-    
-    ofxImGui::Gui gui;
-	
+
+	void setup_ImGui();
+	ofxImGui::Gui gui;
+	ofxImGui::Settings mainSettings = ofxImGui::Settings();
+	ImFont* customFont = nullptr;
+	ofParameter<bool> bGui{ "Show Gui", true };
+	ofParameter<bool> auto_resize{ "Auto Resize", true };
+	ofParameter<bool> bLockMouseByImGui{ "Mouse Locked", false };
+	ofParameter<bool> auto_lockToBorder{ "Lock GUI", false };
+
 	// all the parameters for the scene 
-	ofParameterGroup paramsNested1;
-	ofParameterGroup paramsNested2;
-	ofParameter<bool> fill;
+	ofParameterGroup params;
 	ofParameter<float> lineWidth;
-	ofParameter<ofFloatColor> color;
+	ofParameter<float> separation;
+	ofParameter<float> speed;
 	ofParameter<int> shapeType;
 	ofParameter<int> amount;
-	ofParameter<int> separation;
 	ofParameter<int> size;
+	ofParameterGroup params2;
+	ofParameter<int> shapeType2;
+	ofParameter<int> amount2;
+	ofParameter<int> size2;
+	ofParameterGroup params3;
+	ofParameter<float> lineWidth3;
+	ofParameter<float> separation3;
+	ofParameter<float> speed3;
+	ofParameter<int> shapeType3;
 
-	// the main group container
-	ofParameterGroup params;
-
-	ofxSurfingRandomizer randomizerGroup;
+	ofxSurfingRandomizer data;
 };
